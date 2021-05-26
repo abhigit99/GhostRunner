8a1fb200ca8562c39a76671a8cf346649fe4d934
ashok kumar yadav <y.ashok@1tab.com>
2021-05-26 19:43:24 +0530

    gamedev

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetpos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+
+    public GameObject effect;
+    public Animator camAnim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  
+        }
+        print(speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetpos, speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetpos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetpos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+}
